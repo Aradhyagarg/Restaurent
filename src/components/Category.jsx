b8df5542ab7f13239684ab105e0f2fc7ff6c310d
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2024-06-23 20:39:57 +0530

    Full And Final Done

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -19,22 +19,3 @@ const Category = ({ onCategorySelect }) => {
 };
 
 export default Category;
-/*import React from 'react';
-
-const Category = () => {
-  return (
-    <section className="category-section">
-      <h2>Shop by Category</h2>
-      <h1>Top Category Of Organic Food</h1>
-      <div className="categories">
-        <button>Fruits and Vegetables</button>
-        <button>Bakery</button>
-        <button>Meat and Seafood</button>
-        <button>Beverage</button>
-        <button>Biscuit and Snacks</button>
-      </div>
-    </section>
-  );
-}
-
-export default Category;*/
